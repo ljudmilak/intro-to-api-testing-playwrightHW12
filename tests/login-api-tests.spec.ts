d2c0d4212d0d3f28190b08d1153e11a38ccecec5
AndrewKhv <a.d.khramchenkov@gmail.com>
2026-03-26 19:15:03 +0100

    feat: TL-12 prettier fix

--- tests/login-api-tests.spec.ts
+++ tests/login-api-tests.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 import { LoginDTO } from '../src/dto/LoginDTO'
 
 test.describe('Login api tests', () => {
-  const BaseEndpointURL = 'https://backend.tallinn-learning.ee/login/student';
+  const BaseEndpointURL = 'https://backend.tallinn-learning.ee/login/student'
 
   test('Incorrect login', async ({ request }) => {
     const loginResponse = await request.post(BaseEndpointURL, {
@@ -12,14 +12,13 @@ test.describe('Login api tests', () => {
   })
 
   test('Correct login', async ({ request }) => {
-    console.log(LoginDTO.generateCorrectPair());
+    console.log(LoginDTO.generateCorrectPair())
     const loginResponse = await request.post(BaseEndpointURL, {
       data: LoginDTO.generateCorrectPair(),
     })
 
-    const token = await loginResponse.text();
-    expect(loginResponse.status()).toBe(200);
-    expect(token.length).toBeGreaterThan(0);
-
+    const token = await loginResponse.text()
+    expect(loginResponse.status()).toBe(200)
+    expect(token.length).toBeGreaterThan(0)
   })
 })
