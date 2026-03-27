edb9a59516192f0d7ee655fe112e01a16975b3a3
ljudmila.kend <ljudmila.kend@gmail.com>
2026-03-27 18:38:17 +0200

    feat:HW12:fixed test

--- src/dto/ProductDTO.ts
+++ src/dto/ProductDTO.ts
@@ -1,31 +1,21 @@
 export class ProductDTO {
-  id: number;
-  name: string;
-  price: number;
-  createdAt: string | null;
-  
+  id: number
+  name: string
+  price: number
+  createdAt: string | null
+
   constructor(id: number, name: string, price: number, createdAt: string | null) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.createdAt = createdAt;
+    this.id = id
+    this.name = name
+    this.price = price
+    this.createdAt = createdAt
   }
-  
+
   static generateDefault(): ProductDTO {
-    return new ProductDTO(
-      0,
-      "test lesson 12",
-      100,
-      new Date().toISOString()
-    )
+    return new ProductDTO(0, 'test lesson 12', 100, new Date().toISOString())
   }
 
   static generateCustom(name: string, price: number): ProductDTO {
-    return new ProductDTO(
-      0,
-      name,
-      price,
-      new Date().toISOString()
-    )
-  };
+    return new ProductDTO(0, name, price, new Date().toISOString())
+  }
 }
